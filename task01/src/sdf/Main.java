55037aa906589f8953b4313de5e5b6300ac59b96
ridzy <ridzuanazmi.11@gmail.com>
2023-01-27 14:03:52 +0800

    sdf assessment

--- task01/src/sdf/Main.java
+++ task01/src/sdf/Main.java
@@ -3,13 +3,12 @@ package sdf;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -25,6 +24,7 @@ public class Main {
         // Create a HashMap to store the word and it's appearance
         HashMap<String, Integer> wordCounts = new HashMap<>();
 
+        // Read the file name that is entered in the console
         String file = "";
         if (args.length > 0) {
             file = args[0];
@@ -39,51 +39,57 @@ public class Main {
             String line;
             System.out.print("Enter word to count in file: ");
             String wordFind = scanner.nextLine();
-            System.out.println("word selected: " + wordFind);
+            System.out.println("word selected: " + wordFind + "\n");
 
             while ((line = br.readLine()) != null) {
 
                 // Split the line into words and replace punctuations with white spaces
+                // wash the line again with split()
                 line = line.replaceAll("[^\\p{L}\\p{Z}]", " ");
                 String[] words = line.split("[\\s@&.?$+-]+");
 
+                // Go through each word
                 for (String word : words) {
                     // Print out the word and increment wordCount
                     // System.out.println(word);
                     if (!word.isEmpty()) {
-                        // check if the word is not empty
-                        wordCount++;
+                        wordCount++; // check if the word is not empty
                     }
-
+                    // converts the word into lowercase and the wordfind to lowercase in the
+                    // condition
+                    // to make things simpler since word is free from punctuation
                     if (word.toLowerCase().equals(wordFind.toLowerCase())) {
                         specificWordCount++;
-                    } 
-                    
-                    if (word.length()>1) {
-                        word = word.toLowerCase();
-                        if (wordCounts.containsKey(word)) {
-                            wordCounts.put(word, wordCounts.get(word)+ 1);
+                    }
+
+                    // add each word into the HashMap wordCounts
+                    if (word.length() >= 1) {
+                        word = word.toLowerCase(); // convert the word into lowercase
+                        if (wordCounts.containsKey(word)) { // checks if there is a repeated word if there is, incrementthe count in HashMap
+                            wordCounts.put(word, wordCounts.get(word) + 1);
                         } else {
-                            wordCounts.put(word, 1);
+                            wordCounts.put(word, 1); // initializes new word into the HashMap
                         }
-                    }                    
+                    }
+
                 } // end of for loop
-            }// end of while loop
+
+            } // end of while loop
+
             // print out wordCount, specific word and it's counter, and word frequency
             System.out.println("Word count = " + wordCount);
             System.out.println("The word " + wordFind + ", appears " + specificWordCount + " times");
             double wordFrequecy = specificWordCount / wordCount;
             System.out.printf("The frequency of the word %s is %.3f%n", wordFind, wordFrequecy);
 
-            // sort the wordCounts in descending order
-            System.out.println("The top 10 words by order of appearance");
-            wordCounts = sortByValue(wordCounts);
-            int count = 0;
-            for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
-                if (count < 10) {
-                    System.out.println(entry.getKey() + ": " + entry.getValue());
-                    count++;
-                }
+            // use Comparator to help sort out wordCounts HashMap
+            Comparator<Map.Entry<String, Integer>> comparator = (e1, e2) -> e2.getValue().compareTo(e1.getValue());
+            List<Map.Entry<String, Integer>> sortedWordCounts = wordCounts.entrySet().stream().sorted(comparator).collect(Collectors.toList());
+
+            // List out the top 10 appearance
+            System.out.println("Top 10 words in appearance:");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(sortedWordCounts.get(i).getKey() + ": " + sortedWordCounts.get(i).getValue());
             }
 
         } finally {
@@ -91,23 +97,9 @@ public class Main {
                 fr.close();
                 br.close();
             }
+
         } // end of try-finally block
 
     } // end of main method
 
-    // sortByValue sorts the top 10 words in order using the HashMap
-    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> wordCounts) {
-
-        List<Map.Entry<String, Integer>> list = new LinkedList<>(wordCounts.entrySet());
-        Collections.sort(list, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-
-        HashMap<String, Integer> temp = new LinkedHashMap<>();
-
-        for (Map.Entry<String, Integer> entry : list) {
-            temp.put(entry.getKey(), entry.getValue());
-        }
-
-        return temp;
-
-    }
-}
+}// end of Main class
